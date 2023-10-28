f8c6b39cb1e6f60e6b3f1852311ede8fdf166ba4
Chris Gonzalez <Gonzalez.chris0223@csu.fullerton.edu>
2023-10-27 19:04:37 -0700

    changes made

--- part-2/average.cc
+++ part-2/average.cc
@@ -29,9 +29,6 @@ int main(int argc, char* argv[]) {
 
   if (sum != 0) {
     avg = sum / count;
-  } else {
-    std::cout << "error: you must supply at least one number" << std::endl;
-    return -1;
   }
 
   std::cout << "\naverage = " << avg << std::endl;
